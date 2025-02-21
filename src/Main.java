59620461c051705f8637569a181227aef5366871
sp1deydev <duckhanhthientran@gmail.com>
2025-02-21 11:33:45 +0700

    test method and instance class

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    int x = 5;
+    int y = 10;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void helloMethod() {
+        System.out.println("Hello word");
+    }
+
+    public static void main(String[] args) {
+        Main myObj1 = new Main();  // Object 1
+        Main myObj2 = new Main();  // Object 2
+        System.out.println(myObj1.x);
+        System.out.println(myObj2.y);
+        helloMethod();
     }
 }
